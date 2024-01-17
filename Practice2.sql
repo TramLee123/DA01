7037ec2c9118c777c98c929b702d54f2ab22a48d
TramLee123 <156493520+TramLee123@users.noreply.github.com>
2024-01-16 23:27:58 -0500

    Create Practice2.sql

---
+++ Practice2.sql
@@ -0,0 +1,31 @@
+---ex1---weather-observation-station-3
+SELECT DISTINCT CITY
+WHERE ID%2=0 FROM STATION
+
+---ex2---
+
+---ex3--
+
+---ex4--
+
+---ex5--
+
+
+---ex6---
+
+
+---ex7---
+
+
+---ex8--
+
+---ex9--
+
+
+---ex10--
+
+---ex11--
+
+---ex12--
+
+
