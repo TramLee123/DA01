320e1ea3e6cd482bdcf1872a2d3d3b0c62fa81ff
TramLee123 <156493520+TramLee123@users.noreply.github.com>
2024-01-20 10:49:28 -0500

    Update Practic3.sql

--- Practic3.sql
+++ Practic3.sql
@@ -5,18 +5,38 @@ where Marks > 75
 order by right(name,3),ID
 
 ---ex2----
-
-
-
+SELECT user_id,  
+    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name,2, 4))) AS NAME
+FROM Users
+ORDER BY user_id;
 
 ---ex3----
-
-
-
+SELECT manufacturer,
+       CONCAT('$', ROUND(SUM((total_sales * units_sold) - cogs) / 1000000), ' million') AS Sales_Mil
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM((total_sales * units_sold) - cogs) DESC;
 
 ---ex4----
+SELECT 
+  EXTRACT(month from submit_date) as MONTH,
+  product_id,
+ ROUND( AVG(stars),2) AS avg_stars
+FROM reviews
+GROUP BY EXTRACT(MONTH FROM submit_date), product_id
+ORDER BY month, product_id 
 
 ---ex5----
+SELECT 
+  sender_id,
+  COUNT(message_id) AS count_messages
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) = '8'
+  AND EXTRACT(YEAR FROM sent_date) = '2022'
+GROUP BY sender_id
+ORDER BY count_messages desc
+limit 2;
+
 
 ---ex6----
 
