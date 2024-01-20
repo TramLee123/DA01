f78e6dfbd8bd2ca5dcb3e12434b577cbc5404e5a
TramLee123 <156493520+TramLee123@users.noreply.github.com>
2024-01-19 23:31:20 -0500

    Create Practic3.sql

---
+++ Practic3.sql
@@ -0,0 +1,40 @@
+---ex1---
+select Name
+from STUDENTS
+where Marks > 75
+order by right(name,3),ID
+
+---ex2----
+
+
+
+
+---ex3----
+
+
+
+
+---ex4----
+
+---ex5----
+
+---ex6----
+
+
+---ex7----
+
+
+---ex8----
+
+
+---ex9----
+
+
+---ex10----
+
+
+---ex11----
+
+
+---ex12---
+
