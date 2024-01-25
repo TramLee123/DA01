84a63a5fa5d374eecb69e7b19796368c4926521d
TramLee123 <156493520+TramLee123@users.noreply.github.com>
2024-01-24 22:45:09 -0500

    Update Practice5.sql

--- Practice5.sql
+++ Practice5.sql
@@ -5,7 +5,7 @@ INNER JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
 WHERE CITY.ID IS NOT NULL
 GROUP BY COUNTRY.CONTINENT
 
----ex2---
+---ex2---https://datalemur.com/questions/signup-confirmation-rate
 SELECT
 round(count(texts.email_id)::decimal/COUNT(DISTINCT emails.email_id),2)as activation_rate
 FROM emails
@@ -13,7 +13,7 @@ LEFT JOIN texts
   ON emails.email_id = texts.email_id
   AND texts.signup_action ='Confirmed'
 
----ex3---
+---ex3---https://datalemur.com/questions/time-spent-snaps
 SELECT 
   age.age_bucket, 
   ROUND(100.0 * 
@@ -28,15 +28,47 @@ INNER JOIN age_breakdown AS age
 WHERE activities.activity_type IN ('send', 'open') 
 GROUP BY age.age_bucket;
 
----ex4---
+---ex4---https://datalemur.com/questions/supercloud-customer
+SELECT 
+  customers.customer_id, 
+  COUNT(DISTINCT products.product_category) AS unique_count
+FROM customer_contracts AS customers
+LEFT JOIN products 
+  ON customers.product_id = products.product_id
+GROUP BY customers.customer_id;
+
+---ex5---https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee
+SELECT
+e.employee_id
+,e.name
+,count(e2.reports_to) as reports_count
+,ROUND(AVG(e2.age*1.0),0) as average_age
+FROM employees e INNER JOIN
+employees e2 ON e.employee_id=e2.reports_to
+WHERE e2.reports_to IS NOT NULL
+GROUP BY
+e.employee_id
+,e.name
+ORDER BY e.employee_id
+
+---ex6---https://leetcode.com/problems/list-the-products-ordered-in-a-period
+Select p.product_name, sum(o.unit) as unit from
+Products as p inner join Orders as o
+on p.product_id = o.product_id
+where order_date between '2020-02-01' and '2020-02-29'
+group by o.product_id
+having unit >= 100;
+
+---ex7---leetcode-- sql pages w/ no likes
+SELECT pg.page_id
+FROM pages pg
+LEFT OUTER JOIN page_likes pl
+ON  pg.page_id = pl.page_id
+where pl.page_id is null
 
 
----ex5---
 
 
-
-
----ex6---
 
 
 
