539818c0c914497f90c190f441da62ff2615a29d
TramLee123 <156493520+TramLee123@users.noreply.github.com>
2024-01-29 23:48:56 -0500

    Update Practice7.sql

--- Practice7.sql
+++ Practice7.sql
@@ -128,3 +128,24 @@ WHERE ranking <= 2
 ORDER BY category, ranking;
 
 ---ex8----
+ITH top_10_cte AS (
+  SELECT 
+    artists.artist_name,
+    DENSE_RANK() OVER (
+      ORDER BY COUNT(songs.song_id) DESC) AS artist_rank
+  FROM artists
+  INNER JOIN songs
+    ON artists.artist_id = songs.artist_id
+  INNER JOIN global_song_rank AS ranking
+    ON songs.song_id = ranking.song_id
+  WHERE ranking.rank <= 5
+  GROUP BY artists.artist_name
+)
+
+SELECT artist_name, artist_rank
+FROM top_10_cte
+WHERE artist_rank <= 5;
+
+
+
+
